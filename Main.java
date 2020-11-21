12f2ed4711677ce888dcc38f61ea8ad73ec7ec67
Konsti-dev <kkomenda@stud.hs-heilbronn.de>
2020-11-21 13:31:03 +0100

    init

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * Just a simple class.
+ */
+public class Main {
+    /**
+     * Just a test comment.
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
